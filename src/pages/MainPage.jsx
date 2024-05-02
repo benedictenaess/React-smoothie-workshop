d956850683509545ccaddca6403fb6011d72ee3f
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-05-02 11:53:23 +0200

    MainPage styling

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import smoothieImg from '../assets/images/smoothie.jpg';
+import styles from './MainPage.module.css';
 
 function MainPage() {
   return (
-	<div>MainPage</div>
+	<div className={styles.home_content_container}>
+		<section className={styles.intro_container}>
+			<h1>Welcome to the imaginary smoothie bar</h1>
+			<p>
+				Welcome to "Imaginary Smoothie Bar," where creativity meets
+				refreshment! Dive into a world of flavorful blends crafted from the
+				freshest fruits and wholesome ingredients. Our menu boasts a variety
+				of imaginative concoctions, from classic favorites to unique
+				combinations that will tantalize your taste buds. Join us at
+				"Imaginary Smoothie Bar" and embark on a flavorful journey that
+				celebrates the endless possibilities of nature's bounty!
+			</p>
+		</section>
+		<section className={styles.home_image_container}>
+			<img className={styles.smoothie_image} src={smoothieImg} alt="Smoothie img" />
+		</section>
+	</div>
   )
 }
 
