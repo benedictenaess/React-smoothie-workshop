714bcb8d417ed336175b6c0c1054235241716030
Benedicte Naess <144252770+benedictenaess@users.noreply.github.com>
2024-05-02 11:47:56 +0200

    NavBar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css';
+import Logo from '../../assets/icons/logo.svg';
 
 function NavBar() {
+	const navLinkActiveHandler = ({isActive}) =>{
+		return {
+			color: isActive ?  "#7FFFD4" : "",
+		}
+	}
+
   return (
-	<nav>
-		<div>
+	<nav className={styles.navbar_container}>
+		<div className={styles.logo_container}>
 			<span>
-				
+				<img src={Logo} alt="Website logo" className={styles.logo} />
 			</span>
 		</div>
-		<NavLink to='/'>Home</NavLink>
-		<NavLink to='smoothie'>Smoothie</NavLink>
-		<NavLink to='nutrition'>Nutrition</NavLink>
-		<NavLink to='contact'>Contact</NavLink>
+		<div className={styles.links_container}>
+			<NavLink style={navLinkActiveHandler} to='/'>Home</NavLink>
+			<NavLink style={navLinkActiveHandler} to='smoothie'>Smoothie</NavLink>
+			<NavLink style={navLinkActiveHandler} to='nutrition'>Nutrition</NavLink>
+			<NavLink style={navLinkActiveHandler} to='contact'>Contact</NavLink>
+		</div>
 	</nav>
   )
 }
